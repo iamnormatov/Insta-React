288672189d693cf07f6263128db4dcddd434cac9
New-Bucket <azizjonnormatov437@gmail.com>
2022-12-26 20:35:18 +0500

    index

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Navbar } from "./Navbar";
 export { default as BottomBar } from "./BottomBar";
 export { default as FormRow2 } from "./FormRow2";
 export { default as PostContainer } from "./PostContainer";
+//.
