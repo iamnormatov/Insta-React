33d7ec8240991315afa8afe5051135202dad6450
New-Bucket <azizjonnormatov437@gmail.com>
2022-12-26 20:40:43 +0500

    error

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import Logo from "../assets/img/instagram logo.svg";
 import { Link } from "react-router-dom";
-
+//.
 const Error = () => {
   return (
     <Wrapper>
