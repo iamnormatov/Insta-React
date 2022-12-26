ad29701aef5e83fc7d4bb29a419aeb300078064b
New-Bucket <azizjonnormatov437@gmail.com>
2022-12-26 20:44:04 +0500

    index

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import { store } from "./app/store";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-
+// .
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
