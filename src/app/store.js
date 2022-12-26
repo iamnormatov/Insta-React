fbffb5d09eeb29a1eff3b2f254ddb10c41affa6e
New-Bucket <azizjonnormatov437@gmail.com>
2022-12-26 20:33:54 +0500

    store

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/user/userSlice";
 import feedReucer from '../features/feed/feedSlice';
-
+//.
 export const store = configureStore({
   reducer: {
     user: userReducer,
