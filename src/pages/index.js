7730cd93b78f4d2b46bd5bf7dc21d36216c89675
New-Bucket <azizjonnormatov437@gmail.com>
2022-12-26 20:40:56 +0500

    index

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,4 @@ export { default as Landing } from "./Landing";
 export { default as Error } from "./Error";
 export { default as Register } from './Register';
 export { default as ProtectedRoute } from "./ProtectedRoute";
+//.
